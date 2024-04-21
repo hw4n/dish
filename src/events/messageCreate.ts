7be7439cdf01d1fdfdab3d36782c26ed5ea21a23
Hwanhee Chae <hh@hwan.me>
2024-04-21 16:30:51 +0900

    add logging attachments and stickers

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -3,8 +3,22 @@ import Logger from '../helper/logger';
 
 module.exports = {
     name: Events.MessageCreate,
-    once: true,
     execute(message: Message) {
+        if (!message.content) {
+            if (message.attachments) {
+                const attachments = message.attachments.map(a => ({name: a.name, link: a.url}));
+                for (const attachment of attachments) {
+                    Logger.chat(`${message.author.id} : <attachment> ${attachment.name} (${attachment.link})`);
+                }
+            }
+            if (message.stickers) {
+                const stickers = message.stickers.map(s => ({name: s.name, id: s.id}));
+                for (const sticker of stickers) {
+                    Logger.chat(`${message.author.id} : <sticker> ${sticker.name} (${sticker.id})`);
+                }
+            }
+            return;
+        }
         Logger.chat(`${message.author.id} : ${message.content}`);
     },
 };
