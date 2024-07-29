4d1731cf38e5e194a88a0a839b177d5169ee1237
Hwanhee Chae <hh@hwan.me>
2024-07-29 17:52:30 +0900

    add minute option for destruction

--- src/commands/utility/destruction.ts
+++ src/commands/utility/destruction.ts
@@ -4,13 +4,21 @@ import Logger from '../../helper/logger';
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('destruction')
-    .setDescription('disconnects all user from the voice channel'),
+    .setDescription('disconnects all user from the voice channel')
+    .addIntegerOption(option => option.setName('minute').setDescription('time in minutes to disconnect').setRequired(false)),
     async execute(interaction: any) {
         if (!interaction.guild) return;
-        for (const [memberId, member] of interaction.guild.voiceStates.cache) {
-            member.disconnect();
-            Logger.success(`${interaction.user.id} disconnected ${memberId}`);
-        }
-        await interaction.reply({ content: 'Disconnected all users from the voice channel', ephemeral: true});
+        
+        if (!interaction.options.getInteger('minute'))
+            await interaction.reply({ content: 'Disconnecting all users from the voice channel', ephemeral: true });
+        else
+            await interaction.reply({ content: `Disconnecting all users from the voice channel in ${interaction.options.getInteger('minute')} minutes`, ephemeral: true });
+        
+        setTimeout(async() => {
+            for (const [memberId, member] of interaction.guild.voiceStates.cache) {
+                member.disconnect();
+                Logger.success(`${interaction.user.id} disconnected ${memberId}`);
+            }
+        }, interaction.options.getInteger('minute') * 60 * 1000);
     },
 };
