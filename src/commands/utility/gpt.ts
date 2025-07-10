44bd66a6532518679c52b15b484fd1b2f6c35526
Hwanhee Chae <hh@hwan.me>
2025-07-10 10:50:16 +0900

    add loading thing

--- src/commands/utility/gpt.ts
+++ src/commands/utility/gpt.ts
@@ -45,6 +45,17 @@ module.exports = {
             });
         }
 
+        // 화려한 로딩
+        let waitString = "Generating answer...";
+        const waitEmoji = ["⠋", "⠙", "⠸", "⠴", "⠦", "⠇"];
+        let waitEmojiIndex = 0;
+        const waitInterval = setInterval(() => {
+            interaction.editReply({
+                content: `${waitString} ${waitEmoji[waitEmojiIndex++]}`,
+            });
+            waitEmojiIndex = waitEmojiIndex % waitEmoji.length;
+        }, 500);
+
         // 일단 시스템 프롬프트를 넣기
         const input: ResponseInput = [{ role: "system", content: prompt }];
 
@@ -76,18 +87,28 @@ module.exports = {
 
         Logger.debug("Request ID: " + response.id);
 
-        await interaction.editReply({
-            content: `## [Q] ${question}\n## [A]\n(함수 실행 대기중)`,
-        });
+        if (!response.output) {
+            // 만약 output이 없다면 function call을 안해서 끝났을테니
+            return interaction.editReply({
+                content: `## [Q] ${question}\n## [A]\n${response.output_text}`,
+            });
+        }
 
         // 최초 응답을 다시 넣기
         input.push(response.output[0]);
 
+        let preInputLength = input.length;
         // mcp 같은놈 불렀으면 실행
         for (const todoCall of response.output) {
             if (todoCall.type !== "function_call") {
                 continue;
             }
+
+            waitInterval && clearInterval(waitInterval);
+
+            await interaction.editReply({
+                content: `## [Q] ${question}\n## [A]\n(함수 실행 대기중)`,
+            });
 
             const name = todoCall.name;
             const args = todoCall.arguments;
@@ -107,19 +128,29 @@ module.exports = {
             Logger.debug("Appended input: " + JSON.stringify(input.slice(2)));
         }
 
-        Logger.debug(
-            "Before second request input: " + JSON.stringify(input.slice(2))
-        );
-        // 다시 GPT에게 응답을 요청
-        const response2 = await Local.openai.responses.create({
-            model: "gpt-4.1",
-            input,
-            tools,
-            max_output_tokens: 1500,
-        });
+        let response2;
+        if (input.length > preInputLength) {
+            Logger.debug(
+                "Before second request input: " + JSON.stringify(input.slice(2))
+            );
+            // 다시 GPT에게 응답을 요청
+            response2 = await Local.openai.responses.create({
+                model: "gpt-4.1",
+                input,
+                tools,
+                max_output_tokens: 1500,
+            });
+        }
+
+        waitInterval && clearInterval(waitInterval);
+
+        // 진짜 최종 응답을 가져오기
+        let responseText = response2?.output_text
+            ? response2.output_text
+            : response.output_text;
 
         // 최종 응답 (필요하면 나눠서) 출력
-        let reply = `## [Q] ${question}\n## [A]\n` + response2.output_text;
+        let reply = `## [Q] ${question}\n## [A]\n` + responseText;
         const chunks = reply.match(/[\s\S]{1,2000}/g);
         if (chunks) {
             await interaction.editReply({ content: chunks[0] });
