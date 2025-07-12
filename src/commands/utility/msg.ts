31882ed9bbfe2d46cfdb76fe486efc359322c432
Hwanhee Chae <hh@hwan.me>
2025-07-12 15:16:57 +0900

    copy gpt to msg

--- src/commands/utility/msg.ts
+++ src/commands/utility/msg.ts
@@ -4,7 +4,9 @@ import Local from "../../helper/local";
 import {
     ResponseInputText,
     ResponseInputImage,
+    ResponseInput,
 } from "openai/resources/responses/responses";
+import axios from "axios";
 
 // load prompt.txt to variable
 const fs = require("fs");
@@ -45,52 +47,125 @@ module.exports = {
             });
         }
 
-        const userContent: Array<ResponseInputText | ResponseInputImage> = [
-            { type: "input_text", text: question },
-        ];
+        // 화려한 로딩
+        let waitString = "Generating answer...";
+        const waitEmoji = ["⠋", "⠙", "⠸", "⠴", "⠦", "⠇"];
+        let waitEmojiIndex = 0;
+        const waitInterval = setInterval(() => {
+            interaction.editReply({
+                content: `${waitString} ${waitEmoji[waitEmojiIndex++]}`,
+            });
+            waitEmojiIndex = waitEmojiIndex % waitEmoji.length;
+        }, 500);
 
+        // 일단 시스템 프롬프트를 넣기
+        const input: ResponseInput = [{ role: "system", content: prompt }];
+
+        // 이미지 넣었냐 안넣었냐에 따라 분기해서 처리
         if (image_url) {
-            userContent.push({
-                type: "input_image",
-                image_url: image_url,
-                detail: "auto",
+            input.push({
+                role: "user",
+                content: [
+                    { type: "input_text", text: question },
+                    { type: "input_image", image_url, detail: "auto" },
+                ],
+            });
+        } else {
+            input.push({
+                role: "user",
+                content: [{ type: "input_text", text: question }],
             });
         }
 
-        const stream = await Local.openai.responses.create({
+        const tools = Local.openai_tools;
+
+        // 호출한다음에
+        const response = await Local.openai.responses.create({
             model: "gpt-4.1",
-            input: [
-                { role: "system", content: prompt },
-                {
-                    role: "user",
-                    content: userContent,
-                },
-            ],
-            tools: [],
+            input,
+            tools,
             max_output_tokens: 1500,
-            stream: true,
         });
 
-        let fullResponse = "";
-        for await (const chunk of stream) {
-            if (chunk.type === "response.output_text.delta") {
-                fullResponse += chunk.delta;
+        Logger.debug("Request ID: " + response.id);
+
+        if (!response.output) {
+            // 만약 output이 없다면 function call을 안해서 끝났을테니
+            return interaction.editReply({
+                content: `## [mQ] ${question}\n## [mA]\n${response.output_text}`,
+            });
+        }
+
+        // 최초 응답을 다시 넣기
+        input.push(response.output[0]);
+
+        let preInputLength = input.length;
+        // mcp 같은놈 불렀으면 실행
+        for (const todoCall of response.output) {
+            if (todoCall.type !== "function_call") {
+                continue;
             }
 
-            // 실시간 업데이트
-            if (
-                fullResponse.length < 1900 &&
-                lastEdit.getTime() + 500 < new Date().getTime()
-            ) {
-                await interaction.editReply({
-                    content: `## [mQ] ${question}\n## [mA]\n` + fullResponse,
-                });
-                lastEdit = new Date();
+            waitInterval && clearInterval(waitInterval);
+
+            await interaction.editReply({
+                content: `## [mQ] ${question}\n## [mA]\n(함수 실행 대기중)`,
+            });
+
+            const name = todoCall.name;
+            const args = todoCall.arguments;
+            Logger.debug(
+                `Function call: ${name} with args: ${JSON.stringify(args)}`
+            );
+
+            if (name === "web_search") {
+                try {
+                    const response = await axios.post(
+                        "http://localhost:3000/browser/search",
+                        {
+                            args,
+                        }
+                    );
+
+                    console.log("-----\n" + response.data + "\n-----");
+
+                    input.push({
+                        type: "function_call_output",
+                        call_id: todoCall.call_id,
+                        output: JSON.stringify(response.data),
+                    });
+                    Logger.debug(
+                        "Appended input: " + JSON.stringify(input.slice(2))
+                    );
+                } catch (error) {
+                    Logger.error("Error calling web_search: " + error);
+                }
             }
         }
 
-        // 최종 응답 처리
-        let reply = `## [mQ] ${question}\n## [mA]\n` + fullResponse;
+        let response2;
+        if (input.length > preInputLength) {
+            Logger.debug(
+                "Before second request input: " + JSON.stringify(input.slice(2))
+            );
+            // 다시 GPT에게 응답을 요청
+            response2 = await Local.openai.responses.create({
+                model: "gpt-4.1",
+                input,
+                tools,
+                max_output_tokens: 1500,
+            });
+        }
+
+        waitInterval && clearInterval(waitInterval);
+
+        // 진짜 최종 응답을 가져오기
+        let responseText = response2?.output_text
+            ? response2.output_text
+            : response.output_text;
+
+        // 최종 응답 (필요하면 나눠서) 출력
+        let reply = `## [mQ] ${question}\n## [mA]\n` + responseText;
         const chunks = reply.match(/[\s\S]{1,2000}/g);
         if (chunks) {
             await interaction.editReply({ content: chunks[0] });
